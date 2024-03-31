043c34eb4cc02d29cd28ee71524b406307e7f1fd
Ameshiro77 <512065954@qq.com>
2024-03-31 22:19:48 +0800

    可视化手动筛选做完了

---
+++ SynPipeline/visualize.py
@@ -0,0 +1,184 @@
+# 本程序可视化并手动筛选
+# TODO
+# 记得改filename 逻辑
+import cv2, os
+import json, random
+import numpy as np
+from labels_txt.labels import id_to_obj_dict, id_to_hoi_dict, id_to_verb_dict
+
+
+def click_corner(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        xy = "%d,%d" % (x, y)
+        cv2.circle(img, (x, y), 1, (255, 0, 0), thickness=-1)
+        cv2.putText(
+            img, xy, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 0), thickness=1
+        )
+        print(x, y)
+
+
+def draw(img, tgt_dict):
+    so_count = {}
+    blk = np.zeros(img.shape, np.uint8)  # 透明背景用的
+    # 画box
+    boxes = tgt_dict["annotations"]  # 后面还有用
+    for box_category in boxes:
+        box = box_category["bbox"]
+        category_id = box_category["category_id"]
+        xy1 = (box[0], box[1])
+        xy2 = (box[2], box[3])
+        color = (np.random.random(3) * 0.6 + 0.4) * 255
+        print(color)
+        cv2.rectangle(img, xy1, xy2, color, 2)  # 画人物的框
+        text = id_to_obj_dict[category_id]
+        cv2.rectangle(  # 用于text的背景框
+            blk,
+            (xy1[0], xy1[1]),
+            (xy1[0] + len(text) * 10, xy1[1] + 25),
+            color,
+            -1,
+        )
+        cv2.putText(
+            blk,
+            text,
+            (xy1[0], xy1[1] + 15),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            (0, 0, 0, 0),
+            2,
+        )
+
+    # 画交互
+    for hoi in tgt_dict["hoi_annotation"]:
+        xyxy1 = boxes[hoi["subject_id"]]["bbox"]  # 得到四个点
+        xyxy2 = boxes[hoi["object_id"]]["bbox"]
+        xy1 = (
+            int(sum(xyxy1[0::2]) / 2),
+            int(sum(xyxy1[1::2]) / 2),
+        )  # 找到两个box的中点坐标
+        xy2 = (int(sum(xyxy2[0::2]) / 2), int(sum(xyxy2[1::2]) / 2))
+        # 如果是no interaction ， 画一条红线 ,无文字
+        if hoi["category_id"] == 58:
+            cv2.line(blk, xy1, xy2, (0, 0, 255), 3)
+        else:
+            s_o = (
+                hoi["subject_id"],
+                hoi["object_id"],
+            )  # 记录主体客体对出现此处 调整text用的
+            if so_count.get(s_o) == None:
+                so_count[s_o] = 0
+            else:
+                so_count[s_o] = so_count[s_o] + 1
+            text_pos = (
+                int((xy1[0] + xy2[0]) / 2),
+                int((xy1[1] + xy2[1]) / 2) + 18 * so_count[s_o],
+            )  # 放文字的基准点
+            text = id_to_verb_dict[hoi["category_id"]]
+            cv2.rectangle(  # 用于text的背景框
+                blk,
+                (text_pos[0], text_pos[1]),
+                (text_pos[0] + len(text) * 10, text_pos[1] + 25),
+                (255, 255, 255),
+                -1,
+            )
+            cv2.putText(
+                blk,
+                text,
+                (text_pos[0], text_pos[1] + 15),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                (0, 0, 0, 0),
+                2,
+            )
+            cv2.line(blk, xy1, xy2, (0, 255, 0), 3)
+        cv2.circle(
+            blk,
+            (int((xy1[0] + xy2[0]) / 2), int((xy1[1] + xy2[1]) / 2)),
+            3,
+            (255, 0, 0),
+            -1,
+        )
+        cv2.circle(
+            blk,
+            (xy1[0], xy1[1]),
+            3,
+            (255, 0, 0),
+            -1,
+        )
+        cv2.circle(
+            blk,
+            (xy2[0], xy2[1]),
+            3,
+            (255, 0, 0),
+            -1,
+        )
+    img = cv2.addWeighted(blk, 2, img, 0.85, 0)
+    return img
+
+
+# 此函数对过滤后的图片和标注重排序名称
+def reorder_name(img_dir, json_path):
+    with open(json_path, "r") as f:
+        annotation = json.load(f)
+    # 重命名图片和重排标签 , 二者数量必须相等 否则直接assert
+    imgs = os.listdir(img_dir)
+    assert len(annotation) == len(imgs)
+    for i, img_filename in enumerate(imgs):
+        # 重命名
+        src = os.path.join(img_dir, img_filename)
+        new_name = img_filename[:10] + "{:06d}".format(i + 1) + ".jpg"
+        tgt = os.path.join(img_dir, new_name)
+        os.rename(src, tgt)
+        # 重排标签
+        annotation[i]["file_name"] = new_name
+        annotation[i]["img_id"] = i + 1
+    # 重写
+    with open(json_path, "w") as f:
+        json.dump(annotation, f, indent=2)
+
+
+if __name__ == "__main__":
+    img_dir = "./SynDatasets/train_images"
+    json_path = "./SynDatasets/annotations/train_val.json"
+    # img_dir = r"G:\Code_Project\ComputerVision\no_frills_hoi_det-release_v1\HICO\hico_clean\hico_20160224_det\images\train2015"
+    # json_path = r"G:\Code_Project\ComputerVision\no_frills_hoi_det-release_v1\HICO\hico_clean\hico_20160224_det\annotations\trainval_hico.json"
+
+    # == 先读取标注文件
+    with open(json_path, "r") as f:
+        annotation = json.load(f)
+
+    # 将字典列表转换为字典，以便快速查找
+    annotation_dict = {item["file_name"]: item for item in annotation}
+
+    # == 遍历文件夹 依次读取图片
+    for img_filename in os.listdir(img_dir):
+        # 显示图片
+        target_dict = annotation_dict.get(img_filename)  # 得到了对应图片的标注字典
+        prompt = target_dict.get("prompt").split(",")[0]  # 名字打印到标题上
+        if prompt == None:
+            prompt = "example"
+        img_path = os.path.join(img_dir, img_filename)
+        img = cv2.imread(img_path)
+        img = draw(img, target_dict)
+        print(target_dict)
+        cv2.namedWindow(prompt)
+        cv2.setMouseCallback(prompt, click_corner)
+        cv2.imshow(prompt, img)
+
+        # 对图片进行操作：删除和保留
+        key = cv2.waitKey(0)
+        if key == ord("q"):  # 退出
+            break
+        if key == ord("d"):  # 删除文件
+            os.remove(img_path)
+            del annotation_dict[img_filename]
+
+        cv2.destroyAllWindows()
+
+    # 删除完了后要重新进行写
+    with open(json_path, "w") as f:
+        new_annotation = list(annotation_dict.values())
+        json.dump(new_annotation, f, indent=2)
+
+    # 重排名序
+    reorder_name(img_dir, json_path)
