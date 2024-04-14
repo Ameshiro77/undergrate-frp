ba5313c56599ea2756022ffddd18c74625f32dbd
Ameshiro77 <512065954@qq.com>
2024-04-14 19:40:49 +0800

    change prompt

--- SynPipeline/main.py
+++ SynPipeline/main.py
@@ -41,10 +41,12 @@ def get_prompt(v_o_list:list):
     human = random.choice(["boy", "teenager", "man", "young man", "woman", "young woman"])
     quality = random.choice(["best quality","masterpiece"])
     details = random.choice(["Professional","Vivid Colors"])
-    scene = random.choice(["spacious", "urban", "rustic"])
-    shooting = random.choice(["DSLR","grainy","4K"])
-    shooting2 = random.choice(["warm lighting","blue hour","backlit"])
-    shooting3 = random.choice(["partial view","back view","front view"])
+    #scene = random.choice(["spacious", "urban", "rustic"])
+    scene = random.choice(["realistic"])
+    shooting = random.choice(["DSLR","4K","HD"])
+    shooting2 = random.choice(["warm lighting","blue hour"])
+    #shooting3 = random.choice(["partial view","back view","front view"])
+    shooting3 = random.choice(["Highly detailed"])
     shooting4 = random.choice(["Canon Eos5D","iphone 12"])
     #prompt_prefix = hico_text_label[v_o].replace("person",race+" "+human)+","
     hoi = "" 
@@ -55,9 +57,9 @@ def get_prompt(v_o_list:list):
         hoi = hoi + (get_verb(v_o)) + " and " 
         last_o = v_o[1]
     hoi = hoi[:-4] + get_noun(v_o_list[-1][1])
-    prompt_prefix = "a photo of a " + race + " " + human + " " + hoi + ","  
-    prompt_suffix = quality + "," + details + "," + scene + "," + \
-    shooting + "," + shooting2  + "," + shooting4
+    prompt_prefix = "(a photo of a " + race + " " + human + " " + hoi + "):1.05,"  
+    prompt_suffix = quality + "," + scene + "," + details + "," + \
+    shooting + "," + shooting2  + "," + shooting3 + "," + shooting4
     return prompt_prefix + prompt_suffix
     
 class SynPipeline:
@@ -65,7 +67,7 @@ class SynPipeline:
         self.config_path = config_path
         self.model_checkpoint_path = ckpt_path
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        #self.model, self.preprocess = clip.load("ViT-B/16", device=self.device)
+        self.model, self.preprocess = clip.load("ViT-B/16", device=self.device)
 
     def random_choice(self,start,mode,seq_hoi_id=None):  #随机选择要生成的vo元组列表
         v_o_list = []
@@ -110,25 +112,16 @@ class SynPipeline:
             prompt,
             height=512,
             width=512,
-            num_inference_steps=50,
+            num_inference_steps=75,
             num_images_per_prompt=1,
-            negative_prompt="mutated hands and fingers,poorly drawn hands,deformed,poorly drawn face,floating limbs,extra limb,floating limbs",
+            #negative_prompt="mutated hands and fingers,poorly drawn hands,deformed,poorly drawn face,floating limbs,low quality,",
+            negative_prompt = "low quality,monochrome,skin blemishes,6 more fingers on one hand,deformity,bad legs,malformed limbs,extra limbs,ugly,poorly drawn hands,poorly drawn face,\
+                               extra fingers,mutated hands,mutation,bad anatomy,disfigured,fused fingers"
         ).images
         return imgs
 
     # 2.检测并过滤，然后  3.标注
     def detect_and_filter_and_anno(self, imgs, verbs_objs_tuple_list: list,out_dir,prompt):
-        # 首先，用CLIP过滤
-        # 向量化
-        # with torch.no_grad():
-        #     image_embed = self.preprocess(img).unsqueeze(0).to(self.device)
-        #     text_embed = clip.tokenize(prompt).to(self.device)
-        #     image_features = self.model.encode_image(image_embed)  # 将图片进行编码  # [1,512]
-        #     text_features = self.model.encode_text(text_embed)  # 将文本进行编码
-        #     similarity = torch.nn.functional.cosine_similarity(image_features, text_features).item()
-        #     if similarity < 0.275:
-        #         return
-        
         # == 读取json 为了之后的标注
         with open("./SynDatasets/annotations/train_val.json","r") as f:
             anno = json.load(f)
@@ -138,15 +131,25 @@ class SynPipeline:
         formatted_name = "Syn_train_" + '{:06d}'.format(files_num + 1) + ".jpg"
         # == 对每个图像检测
         for img in imgs:
+            # ========= 首先，用CLIP过滤
+            with torch.no_grad():
+                image_embed = self.preprocess(img).unsqueeze(0).to(self.device) # 向量化
+                text_embed = clip.tokenize(prompt).to(self.device)
+                image_features = self.model.encode_image(image_embed)  # 将图片进行编码  # [1,512]
+                text_features = self.model.encode_text(text_embed)  # 将文本进行编码
+                similarity = torch.nn.functional.cosine_similarity(image_features, text_features).item()
+                if similarity < 0.25:
+                    print("CLIP检测不通过")
+                    return
+            # ===== 然后检测目标，过滤，标注
             tgt = detect(img, self.config_path, self.model_checkpoint_path)
-            # 过滤
-            # print("tgt:",tgt)
-            # == 检查是否检测出了指定的物体
+            # 检查是否检测出了指定的物体
             for v_o in verbs_objs_tuple_list:
                 obj_id = v_o[1]
                 if obj_id not in tgt["box_label_parse_id"]:
                     #print(tgt)
                     #raise ValueError("错误！没检测出生成图片时指定的物体！")
+                    print("没检测出指定物体")
                     return
             # == 如果检测出来就做标注,并保存
             new_anno = generate_annotation(verbs_objs_tuple_list,tgt,formatted_name,files_num + 1) #先获得标注框
@@ -173,10 +176,12 @@ class SynPipeline:
                 print("目前进度:"+str(i+1)+"/"+str(imgs_num))
         if mode == 'seq':
             count = 0
+            _rare_list = rare_list
+            random.shuffle(_rare_list)
             sum = rare_num * imgs_num
             for i in range(rare_num):
                 for j in range(imgs_num):
-                    v_o_list = self.random_choice(rare_num,mode,rare_list[i])
+                    v_o_list = self.random_choice(rare_num,mode,_rare_list[i])
                     prompt = get_prompt(v_o_list)
                     imgs = pipeline.generate(SDpipe,prompt) 
                     pipeline.detect_and_filter_and_anno(imgs,v_o_list,out_dir,prompt)
